ff85255af1561b112d6134fd6b07cfc21bd992e4
damilola <oyeyipo45@gmail.com>
2022-02-11 11:34:23 +0100

    create task form

---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputField = () => {
+  return (
+      <form className='input'>
+          <input type='input' placeholder='Enter a task' className='input__box' />
+          <button className='input__submit' type="submit"> Go</button>
+    </form>
+  )
+}
+
+export default InputField
